aef3f8d9263f5e0ddc7477d60d5a5422dfd0c826
shosuke-13 <begonia.sp.13@gmail.com>
2024-09-04 16:25:43 +0900

    add experiments

---
+++ finetune/scripts/dok/exp2_splicing.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# nucleotide-transformer models
+models=(
+    "InstaDeepAI/nucleotide-transformer-500m-human-ref"
+    "InstaDeepAI/nucleotide-transformer-500m-1000g"
+    "InstaDeepAI/nucleotide-transformer-500m-1000g"
+    "InstaDeepAI/nucleotide-transformer-2.5b-1000g"
+    "InstaDeepAI/nucleotide-transformer-2.5b-multi-species"
+    "InstaDeepAI/agro-nucleotide-transformer-1b"
+)
+
+# 10 tasks
+# (IA)^3 LR=3e-3 
+# (3e-3, paper) Few-Shot Parameter-Efficient Fine-Tuning is Better and Cheaper than In-Context Learning
+for model in "${models[@]}"
+do
+    python train.py \
+        --hf_model_path "$model" \
+        --hf_dataset_repo InstaDeepAI/plant-genomic-benchmark \
+        --task_name "splicing" \
+        --output_dir "$SAKURA_ARTIFACT_DIR" \
+        --project_name "$PROJECT_NAME" \
+        --use_lora False \
+        --use_ia3 True \
+        --use_nt_kmer True \
+        --per_device_train_batch_size "$BATCH_SIZE" \
+        --per_device_eval_batch_size "$BATCH_SIZE" \
+        --num_train_epochs 3 \
+        --learning_rate 3e-3 \
+        --warmup_ratio 0.1 \
+        --save_strategy "epoch" \
+        --evaluation_strategy "epoch" \
+        --logging_steps 15000 \
+        --fp16 True \
+        --report_to "wandb" \
+        --seed 42
+done
---
+++ finetune/scripts/dok/exp2_tasks.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+# plant-genomic-benchmark
+tasks=(
+    "promoter_strength"
+    "terminator_strength"
+    "lncrna"
+    "poly_a"
+)
+
+# nucleotide-transformer models
+models=(
+    "InstaDeepAI/nucleotide-transformer-500m-human-ref"
+    "InstaDeepAI/nucleotide-transformer-500m-1000g"
+    "InstaDeepAI/nucleotide-transformer-500m-1000g"
+    "InstaDeepAI/nucleotide-transformer-2.5b-1000g"
+    "InstaDeepAI/nucleotide-transformer-2.5b-multi-species"
+    "InstaDeepAI/agro-nucleotide-transformer-1b"
+)
+
+# 80 tasks
+# (IA)^3 LR=3e-3 
+# (3e-3, paper) Few-Shot Parameter-Efficient Fine-Tuning is Better and Cheaper than In-Context Learning
+for task in "${tasks[@]}"
+do
+    for model in "${models[@]}"
+    do
+        for seed in {40..44}
+        do
+            python train.py \
+                --hf_model_path "$model" \
+                --hf_dataset_repo InstaDeepAI/plant-genomic-benchmark \
+                --task_name "$task" \
+                --output_dir "$SAKURA_ARTIFACT_DIR" \
+                --project_name "$PROJECT_NAME" \
+                --use_lora False \
+                --use_ia3 True \
+                --use_nt_kmer True \
+                --per_device_train_batch_size "$BATCH_SIZE" \
+                --per_device_eval_batch_size "$BATCH_SIZE" \
+                --num_train_epochs "$EPOCHS" \
+                --learning_rate 3e-3 \
+                --warmup_ratio 0.1 \
+                --save_strategy "epoch" \
+                --evaluation_strategy "epoch" \
+                --logging_steps 10000 \
+                --fp16 True \
+                --report_to "wandb" \
+                --seed "$seed"
+        done
+    done
+done
